3eb9c3765b8ef53462bb0b7e6031c1bee07b30f2
plpcm <caomiao009@gmail.com>
2021-10-09 20:29:39 +0800

    2021-10-09 20:29:39

---
+++ cron.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# author: plpcm
+
+#设置用户名
+git config --global user.name "plpcm" 
+#设置邮箱
+git config --global user.email "caomiao009@gmail.com" 
+
+date +"%F %T" >> daily_data.txt
+echo "My scheduled tasks are completed" >> daily_data.txt
+
+#git status -s
+git status
+
+git add daily_data.txt
+git commit -m "`date +'%F %T'`"
+git push
+
+# end
